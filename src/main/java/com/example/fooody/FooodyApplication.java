0d27787184719cba42b5f55c9e9e174b6a7d2b4c
avelyy <dldkwls3221@naver.com>
2022-08-09 15:43:34 +0900

    Initial commit

---
+++ src/main/java/com/example/fooody/FooodyApplication.java
@@ -0,0 +1,13 @@
+package com.example.fooody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooodyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FooodyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/fooody/FooodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fooody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooodyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
