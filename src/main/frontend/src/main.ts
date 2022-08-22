157c31ddc6a32545cb554c41e103ca03337e62f5
avelyy <dldkwls3221@naver.com>
2022-08-22 17:21:45 +0900

    Vuestic UI 적용
    https://vuestic.dev/

--- src/main/frontend/src/main.ts
+++ src/main/frontend/src/main.ts
@@ -3,8 +3,11 @@ import './style.css'
 import App from './App.vue'
 import router from './router/router'
 import store from './store/store'
+import { createVuestic } from 'vuestic-ui'
+import 'vuestic-ui/css'
 
 createApp(App)
   .use(router)
   .use(store)
+  .use(createVuestic())
   .mount('#app')
--- src/main/frontend/src/views/Home.vue
+++ src/main/frontend/src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
-  This is Home Page<br>
-<!--  <router-link to="/">Home</router-link><br>
-  <router-link to="/test">Test</router-link>-->
+  <div class="row">
+    <div class="flex md12">
+      This is Home Page
+    </div>
+    <div class="flex md12">
+      <va-button color="info" class="mt-2 mb-2 mr-2 align--center">Login</va-button>
+      <va-button color="info" class="mt-2 mb-2 align--center">GO</va-button>
+    </div>
+  </div>
+  <br>
+
 </template>
 
 <script setup lang="ts">
